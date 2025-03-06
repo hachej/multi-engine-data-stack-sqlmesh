ac16228622c2840cce6d3802ae0f8db1359c77a4
huraultj <julien.hurault@sumeo.io>
2025-03-06 14:04:14 +0100

    update

--- sqlmesh/models/metric_1.sql
+++ sqlmesh/models/metric_1.sql
@@ -1,17 +1,17 @@
 MODEL (
-  name multiengine.metrics,
+  name multiengine.metric_1,
   gateway athena,
   start '2025-03-05 19:20:00',
   cron '@hourly',
   physical_properties (
     table_format=iceberg,
-    s3_base_location='s3://sumeo-parquet-data-lake/multiengine/metrics/'
+    s3_base_location='s3://sumeo-parquet-data-lake/multiengine/metric_1/'
   )
 );
 
 SELECT
    action, count(*) as count
 FROM
-  events
+  multiengine.events
 GROUP BY
     action
